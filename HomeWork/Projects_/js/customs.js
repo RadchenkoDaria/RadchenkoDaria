b7142030c3d030285c5e766a397c7d853196cc20
RadchenkoDaria <dajha@ukr.net>
2021-01-26 18:47:28 +0200

    HW35

--- HomeWork/Projects_/js/customs.js
+++ HomeWork/Projects_/js/customs.js
@@ -58,5 +58,53 @@ jQuery(document).ready(function () {
     nav: true
 	})
 
+	/*ДЛЯ ПРОЕКТА С ЭЛЬЗОЙ*/
+	var slid1 = jQuery('#slider-baner3');
+		
+	slid1.owlCarousel({
+		items: 1,
+		autoplay: false,
+		autoplayTimeout: 6000,
+		autoplayHoverPause: false,
+		smartSpeed: 1000,
+		dotsSpeed: 1000,
+		loop: true,
+		nav: true,
+		navText: ['', ''],
+		dots: false,
+		mouseDrag: true,
+		margin: 5,
+		stagePadding: 0,
+		autoWidth: false,
+		responsiveClass: true,
+		responsive:{
+			0:{
+				items: 2,
+				nav: false,
+				mouseDrag: true,
+				autoplay: false
+			},
+			479:{
+				items: 2,
+				mouseDrag: true
+			},
+			629:{
+				items: 3,
+				mouseDrag: true
+			},
+			767:{
+				items: 3,
+				mouseDrag: true
+			},
+			991:{
+				items: 4,
+				mouseDrag: true
+			},
+			1099:{
+				items: 6,
+				mouseDrag: true
+			}
+		}
+	});
 	});
 	
